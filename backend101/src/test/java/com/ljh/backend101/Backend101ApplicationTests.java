79a979efa5a8885707bf31b09b89a880a2c1d146
lijiahong <488241817@qq.com>
2024-07-16 19:32:01 +0800

    创建springboot项目

---
+++ backend101/src/test/java/com/ljh/backend101/Backend101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljh.backend101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backend101ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
