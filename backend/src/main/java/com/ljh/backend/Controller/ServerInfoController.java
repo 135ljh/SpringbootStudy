8c5ad3399cb2816904536c2254941ae8b7cba4d5
lijiahong <488241817@qq.com>
2024-07-16 21:13:23 +0800

    使用了从配置中获取数据的方法，@Value注入，但跑出来文本是直接打印在页面上

---
+++ backend/src/main/java/com/ljh/backend/Controller/ServerInfoController.java
@@ -0,0 +1,18 @@
+package com.ljh.backend.Controller;
+
+import com.ljh.backend.Dto.ServerInfoDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServerInfoController {
+    @Value("${message}")
+    private String msg;
+    @GetMapping("/server_info")
+    String serverInfo() {
+        return "{\"message\": \"" + msg + "\"}";
+    }
+
+
+}
