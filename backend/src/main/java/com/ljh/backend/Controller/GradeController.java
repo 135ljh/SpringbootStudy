9d9cf6e1eec7565df78e0121e853c7d5af1146ca
lijiahong <488241817@qq.com>
2024-07-17 12:11:43 +0800

    完成查询所有成绩功能

--- backend/src/main/java/com/ljh/backend/Controller/GradeController.java
+++ backend/src/main/java/com/ljh/backend/Controller/GradeController.java
@@ -3,13 +3,22 @@ package com.ljh.backend.Controller;
 
 
 import com.ljh.backend.Dto.GradeDto;
+import com.ljh.backend.domain.User;
+import com.ljh.backend.mapper.GradeMapper;
+import com.ljh.backend.mapper.UserMapper;
+import com.ljh.backend.model.Grade;
 import com.ljh.backend.service.GradeService;
 import org.apache.ibatis.javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class GradeController {
+    @Autowired
+    private GradeMapper gradeMapper;
+
     private final GradeService gradeService;
 
 
@@ -23,16 +32,26 @@ public class GradeController {
         this.gradeService = gradeService;
     }
 
+    /*@RequestParam注解说明该方法通过请求参数的方式获取输入的studentid，用于指定要查询成绩的学生的ID。*/
     @GetMapping("/grade")
     public GradeDto getGrade(@RequestParam String studentid) throws NotFoundException {
         return gradeService.getGrade(studentid);
     }
 
+    /*@RequestBody注解表示该参数是从请求体中获取的*/
     @PostMapping("/add_grade")
     public GradeDto addGrade(@RequestBody GradeDto grade) {
         return gradeService.addGrade(grade);
     }
 
+
+    @GetMapping("/all_grades")
+    public List<Grade> FindAll() {
+        List<Grade> grades = gradeMapper.getAllGrades();
+        System.out.println(grades);
+        return grades;
+
+    }
 
 
 }
