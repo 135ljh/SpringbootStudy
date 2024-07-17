d40519d634a159a24efa6da2844edac1bc6c1643
lijiahong <488241817@qq.com>
2024-07-17 10:13:42 +0800

    写了添加成绩的接口和查询所有的接口

--- backend/src/main/java/com/ljh/backend/mapper/GradeMapper.java
+++ backend/src/main/java/com/ljh/backend/mapper/GradeMapper.java
@@ -1,13 +1,22 @@
 package com.ljh.backend.mapper;
 
 import com.ljh.backend.model.Grade;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface GradeMapper {
     @Select("select * from grades where studentid=#{studentid}")
     Grade getGradeById(String studentid);
+
+    @Insert("insert into grades (studentid, grade) values (#{studentid}, #{grade})")
+    void addGrade(Grade grade);
+
+    @Select("select * from grades")
+    List<Grade> getAllGrades();
 
 
 
