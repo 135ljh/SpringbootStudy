7bdaa57ee2e70207fa78c839587e0337c943dc6e
lijiahong <488241817@qq.com>
2024-07-17 09:57:04 +0800

    写了学生成绩类Grade和GradeDto

---
+++ backend/src/main/java/com/ljh/backend/Dto/GradeDto.java
@@ -0,0 +1,10 @@
+package com.ljh.backend.Dto;
+
+import lombok.Data;
+
+@Data
+public class GradeDto {
+    private String studentid;
+    private int grade;
+
+}
---
+++ backend/src/main/java/com/ljh/backend/model/Grade.java
@@ -0,0 +1,9 @@
+package com.ljh.backend.model;
+
+import lombok.Data;
+
+@Data
+public class Grade {
+    private String studentid;
+    private int grade;
+}
