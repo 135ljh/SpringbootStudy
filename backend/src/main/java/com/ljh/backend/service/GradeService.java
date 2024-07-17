af21c556dba920abe1568f8cbbae11ef1da8a9b0
lijiahong <488241817@qq.com>
2024-07-17 11:04:13 +0800

    GradeService类中，完成getGrade方法和addGrade方法，均返回GradeDto类型对象，对象映射分别用了MapStruct和ModelMapper两种方法

--- backend/src/main/java/com/ljh/backend/service/GradeService.java
+++ backend/src/main/java/com/ljh/backend/service/GradeService.java
@@ -3,11 +3,13 @@ package com.ljh.backend.service;
 import com.ljh.backend.Dto.GradeDto;
 import com.ljh.backend.mapper.GradeMapper;
 import com.ljh.backend.model.Grade;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GradeService {
     private final GradeMapper gradeMapper;
+    private final ModelMapper modelMapper = new ModelMapper();
 
     public GradeService(GradeMapper gradeMapper) {
         this.gradeMapper = gradeMapper;
@@ -17,6 +19,13 @@ public class GradeService {
     public GradeDto getGrade(String studentid) {
         Grade grade = gradeMapper.getGradeById(studentid);
         return grade != null ? gradeMapper.toDto(grade) : null;
+
+        /*return modelMapper.map(gradeMapper.getGradeById(studentid), GradeDto.class);*/
+    }
+
+    public GradeDto addGrade(GradeDto grade) {
+        gradeMapper.addGrade(modelMapper.map(grade, Grade.class));
+        return grade;
     }
 
 
