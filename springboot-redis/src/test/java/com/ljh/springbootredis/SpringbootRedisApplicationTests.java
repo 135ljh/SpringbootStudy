d17a7859ed5446c5d7b562dbbc795929f32f90ca
lijiahong <488241817@qq.com>
2024-07-23 15:16:52 +0800

    springboot整合redis

---
+++ springboot-redis/src/test/java/com/ljh/springbootredis/SpringbootRedisApplicationTests.java
@@ -0,0 +1,28 @@
+package com.ljh.springbootredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@SpringBootTest
+class SpringbootRedisApplicationTests {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    public void testSet() {
+        //存入数据
+        redisTemplate.boundValueOps("name").set("zhangsan");
+
+    }
+
+    @Test
+    public void testGet() {
+        //获取数据
+        Object name = redisTemplate.boundValueOps("name").get();
+        System.out.println(name);
+    }
+
+}
